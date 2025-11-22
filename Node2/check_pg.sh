020f8ef88acc844ba3c5336532557d2fa4678863
rez <153321625+rezaamb@users.noreply.github.com>
2025-11-22 15:32:54 +0330

    Update check_pg.sh

--- Node2/check_pg.sh
+++ Node2/check_pg.sh
@@ -1,6 +1,5 @@
 # /usr/local/bin/check_pg.sh
 
 #!/bin/bash
-# چک ﻡی<200c>کﻥیﻡ کﺎﻨﺗیﻥﺭ pg-standby ﺩﺭ ﺡﺎﻟ پﺎﺴﺧ<200c>گﻭیی ﺎﺴﺗ
 docker exec pg-standby pg_isready -U postgres -d postgres >/dev/null 2>&1 || exit 1
 exit 0
